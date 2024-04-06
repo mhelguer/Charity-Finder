9ba11c6b53220216a87b9070d42cc1756fb08a0b
Miguel <miguelihelguero@gmail.com>
2024-04-06 13:39:08 -0500

    finished styling section to search for charities

--- obj/Debug/net8.0/CharityFinder.AssemblyInfo.cs
+++ obj/Debug/net8.0/CharityFinder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CharityFinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2049dc2ac44a90113570e33ab02acda3a61d67c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85b42f1e6633870b34e37fdb8c9df8b6c1ce119b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CharityFinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CharityFinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
