8d33564ba4dd74574712e1ce6411ae1ebe13d232
Miguel <miguelihelguero@gmail.com>
2024-04-15 12:04:58 -0500

    finished featured section styling

--- obj/Debug/net8.0/CharityFinder.AssemblyInfo.cs
+++ obj/Debug/net8.0/CharityFinder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CharityFinder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b83f4de7510fcea419fb7eaa6ad1f58813491bff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4afb068ff2fed6a844e51f60fef47a91b9319eeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CharityFinder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CharityFinder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
