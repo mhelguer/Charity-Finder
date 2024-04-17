6a47f43c9339745d210f1705b516b2bbc3a62522
Miguel <miguelihelguero@gmail.com>
2024-04-16 19:28:16 -0500

    adding Charity-Finder and UnitTests folders

---
+++ Charity-Finder/Pages/Results.cshtml.cs
@@ -0,0 +1,25 @@
+using CharityFinder.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace CharityFinder.Pages
+{
+    public class ResultsModel : PageModel
+    {
+        public List<Charity> CharitiesObj { get; set; }
+        public void OnGet()
+        {
+            var jsonString = HttpContext.Session.GetString("CharitiesObj");
+            if (!string.IsNullOrEmpty(jsonString))
+            {
+                CharitiesObj = JsonConvert.DeserializeObject<List<Charity>>(jsonString);
+                ViewData["CharitiesObj"] = CharitiesObj;
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
